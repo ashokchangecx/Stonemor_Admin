c6bb7a66917348ab5928e8cb124c840c5c716446
Rajapandi <rajapandi.k@changecx.com>
2023-01-03 11:59:57 +0530

    Questionnarie name added in report

--- src/utils/Excel.js
+++ src/utils/Excel.js
@@ -1,16 +1,13 @@
 import moment from "moment";
 
-export const SurveyEntriesToExcel = (surveyEntries) =>
-  surveyEntries.map((entry) => {
-    const {
-      id,
-      questionnaireId,
-      finishTime,
-      startTime,
-      by,
-      location,
-      createdAt,
-    } = entry;
+export const SurveyEntriesToExcel = (surveyEntries, questionariesName) => {
+  const getQuestionarieName = (qid) => {
+    const question = questionariesName?.find((q) => q?.id === qid);
+    return question ? question?.name : qid;
+  };
+  return surveyEntries.map((entry) => {
+    const { questionnaireId, finishTime, startTime, by, location, createdAt } =
+      entry;
     const SD = moment(startTime);
     const ED = moment(finishTime);
     const duration = ED.diff(SD, "seconds");
@@ -20,13 +17,13 @@ export const SurveyEntriesToExcel = (surveyEntries) =>
     const locationName = location?.location || "-";
     const locationInchargeMail = location?.inchargeEmail || "-";
     return {
-      QuestionnaireId: questionnaireId,
+      Questionnaire: getQuestionarieName(questionnaireId),
       Date: created,
-      duration: duration,
+      Duration: duration,
       UserName: userName,
       UserEmail: userEmail,
       LocationName: locationName,
       LocationMail: locationInchargeMail,
-      SurveyEntryId: id,
     };
   });
+};
